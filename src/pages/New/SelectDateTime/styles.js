cfa2a974f4b0baa5a7013908fdb2b41f6c3fa226
EduardoAraujoB <ea0123450@gmail.com>
2019-11-11 17:54:34 -0300

    now user can select an hour and a date in the page to made a new appointment

--- src/pages/New/SelectDateTime/styles.js
+++ src/pages/New/SelectDateTime/styles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
+`;
+
+export const HourList = styled.FlatList.attrs({
+  numColumns: 2,
+  showsVerticalScrollIndicator: false,
+})`
+  padding: 0 20px;
+`;
+
+export const Hour = styled(RectButton)`
+  background: #fff;
+  padding: 20px;
+  opacity: ${props => (props.enabled ? 1 : 0.6)};
+  flex: 1;
+  border-radius: 5px;
+
+  align-items: center;
+  margin: 0 10px 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
 `;
