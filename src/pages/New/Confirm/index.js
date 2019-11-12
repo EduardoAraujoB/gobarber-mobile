cc11ecbd4fe1f6df07fb35a349e1674b176091f5
EduardoAraujoB <ea0123450@gmail.com>
2019-11-12 13:33:23 -0300

    created screen to submit a new appointment

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,73 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function Confirm() {
-  return <View />;
+import Background from '~/components/Background';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormated = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('/appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? `http://192.168.100.18:3333/files/${provider.avatar.path}`
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+
+        <Time>{dateFormated}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar Agendamendo
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar Agendamendo',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={25} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
+
+Confirm.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func.isRequired,
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
